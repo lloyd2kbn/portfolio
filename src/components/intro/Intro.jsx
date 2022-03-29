caf5954f285cc809573f14a0a281b9e8eb2057d8
hiệp hoàng <18130077@st.hcmuaf.edu.vn>
2022-03-29 21:34:59 +0700

    push

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,31 @@
+import './intro.scss'
+import { init } from 'ityped'
+import { useEffect,useRef } from 'react'
+function Intro() {
+  const textRef=useRef();
+  useEffect(()=>{
+    init(textRef.current, { showCursor: false,  backDelay:  1500,strings: ['Developer', ' Designer','Content Creator' ] })
+  
+  },[])
+  return (
+    <div className='intro' id='intro'>
+          <div className='left'>
+              <div className="imgContainer">
+                      <img src="assets/man.png" alt="" />
+              </div>
+          </div>
+          <div className='right'>
+            <div className='wrapper'>
+                    <h2>Hi there, I'm</h2>
+                    <h1>Safak Kocaoluh</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+            </div>
+            <a href="#portfolio">
+                <img src="assets/down.png" alt="" />
+            </a>
+          </div>
+    </div>
+  )
+}
+
+export default Intro
